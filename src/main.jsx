bf08b725a3c154f75d7b7be240772f5ddd233289
jnidv <jannealvillamin@gmail.com>
2024-07-03 23:49:54 +0800

    feat: added and styled sign up page

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import SignIn from "./screens/SignIn.jsx";
+import SignUp from "./screens/SignUp.jsx";
 
-const router = createBrowserRouter([{ path: "/", element: <SignIn /> }]);
+const router = createBrowserRouter([
+    { path: "/", element: <SignIn /> },
+    { path: "/sign-up", element: <SignUp /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
---
+++ src/screens/SignUp.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "../css/SignUp.css";
+
+function SignUp() {
+    return (
+        <>
+            <div className="sign-up-box">
+                <p className="sign-up-label">SIGN UP</p>
+                <div className="input-fields-container">
+                    <input
+                        className="first-name-input"
+                        type="text"
+                        placeholder="First name"
+                    ></input>
+                    <input
+                        className="middle-name-input"
+                        type="text"
+                        placeholder="Middle name (optional)"
+                    ></input>
+                    <input
+                        className="last-name-input"
+                        type="text"
+                        placeholder="Last name"
+                    ></input>
+                    <input
+                        className="email-input"
+                        type="email"
+                        placeholder="Email"
+                    ></input>
+                    <input
+                        className="password-input"
+                        type="password"
+                        placeholder="Password"
+                    ></input>
+                    <input
+                        className="repeat-password-input"
+                        type="password"
+                        placeholder="Repeat password"
+                    ></input>
+                    <button className="sign-up-button">Sign Up</button>
+                    <p className="question">Already have an account?</p>
+                    <button className="sign-in">SIGN IN</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignUp;
