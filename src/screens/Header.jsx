f2560c11396f6820fc9379bd3c5f2b743b277f4e
jnidv <jannealvillamin@gmail.com>
2024-07-10 15:25:17 +0800

    feat: finished header and added media queries for resizing

--- src/screens/Header.jsx
+++ src/screens/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../css/Header.css";
 import NoTitleLogo from "../assets/NoTitleLogo.png";
+import { IconContext } from "react-icons";
+import { FaUser } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
 
 function Header() {
     return (
@@ -8,13 +11,32 @@ function Header() {
             <div className="header">
                 <nav className="navigation-buttons">
                     <img className="logo-button" src={NoTitleLogo}></img>
-                    <p>Info</p>
-                    <p>DFS</p>
-                    <p>BFS</p>
-                    <p>Dijkstra</p>
-                    <p>A*</p>
-                    <p>About dev</p>
+                    <p className="info-navigation-button">Info</p>
+                    <p classNmae="dfs-navigation-button">DFS</p>
+                    <p className="bfs-navigation-button">BFS</p>
+                    <p className="dijkstra-navigation-button">Dijkstra</p>
+                    <p className="a-star-navigation-button">A*</p>
+                    <p className="about-dev-navigation-button">About dev</p>
                 </nav>
+                <div className="friends-profile-sign-out">
+                    <IconContext.Provider
+                        value={{
+                            color: "white",
+                            className: "friends-header-button",
+                        }}
+                    >
+                        <FaUserFriends />
+                    </IconContext.Provider>
+                    <IconContext.Provider
+                        value={{
+                            color: "white",
+                            className: "profile-header-button",
+                        }}
+                    >
+                        <FaUser />
+                    </IconContext.Provider>
+                    <button className="sign-out-button">Sign Out</button>
+                </div>
             </div>
         </>
     );
